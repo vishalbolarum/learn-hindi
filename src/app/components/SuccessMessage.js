bfb9d5c0bce4663e01ae3f58119fa396a7500e62
Vishal Bolarum <vishal@kyroc.co.uk>
2025-07-20 16:38:12 +0530

    enter key

--- src/app/components/SuccessMessage.js
+++ src/app/components/SuccessMessage.js
@@ -8,7 +8,7 @@ export default function SuccessMessage({ close, fetchTask }) {
     }
 
     return (
-        <div className="fixed left-0 top-0 h-screen w-screen backdrop-blur bg-white/20 flex items-center justify-center z-10">
+        <div className="fixed left-0 top-0 h-screen w-screen backdrop-blur bg-white/20 flex items-center justify-center z-10" onKeyDown={e => e.key === "Enter" ? nextTask() : ""}>
             <div className="bg-black p-4 min-w-96">
                 <h2 className="text-2xl">Correct!</h2>
                 <div className="py-4"></div>
