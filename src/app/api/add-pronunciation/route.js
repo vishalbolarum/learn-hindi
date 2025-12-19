079014c5845276322f344583776a627ad48e27ac
Vishal Bolarum <vishal@kyroc.co.uk>
2025-12-19 14:54:37 +0530

    ignore words

--- src/app/api/add-pronunciation/route.js
+++ src/app/api/add-pronunciation/route.js
@@ -3,12 +3,14 @@ import knex from "../_database/knex"
 
 export async function POST(req) {
     const body = await req.json()
-    let { hi, en_transliteration } = body
+    let { hi, en_transliteration, ignore } = body
     hi = hi?.trim()
     en_transliteration = en_transliteration?.trim()?.toLowerCase()
-    await knex("words").insert({ hi, en_transliteration }).onConflict().ignore()
+    ignore = !!ignore || null
+    await knex("words").insert({ hi, en_transliteration, ignore }).onConflict("hi").merge(["ignore"])
     return NextResponse.json({
       hi,
-      en_transliteration
+      en_transliteration,
+      ignore
     })
 }
